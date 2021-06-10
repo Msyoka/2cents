5598ada1cc69364e61b75e09cd8767c63e846243
Msyoka <daviemumo37@gmail.com>
2021-06-10 17:26:31 +0300

    work on forms and admin

--- podcast/admin.py
+++ podcast/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Profile, Comments, Votes, Projects
+
+# Register your models here.
+admin.site.register(Projects)
+admin.site.register(Profile)
+admin.site.register(Comments)
+admin.site.register(Votes)
